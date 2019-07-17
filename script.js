be7df9ae391a035b66eb36e0cf91bb258e77d49a
Jamie Miller <miller0328@gmail.com>
2019-07-16 20:13:27 -0400

    fixed score counter in js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("testing")
+
+//////
+var score = 0;
+
+document.querySelector("#start").addEventListener("click", function(evt) {
+	evt.preventDefault();
+	document.querySelector("#score").innerText = "Score: " + score;
+});
+
